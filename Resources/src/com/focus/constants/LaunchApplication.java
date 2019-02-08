929038b17fcffe3041e7239d09a2fbd7e1a9cbd4
ananya <ananya@focussoftnet.com>
2019-02-08 17:56:32 +0530

    In Launth Application commented code removed

--- Resources/src/com/focus/constants/LaunchApplication.java
+++ Resources/src/com/focus/constants/LaunchApplication.java
@@ -36,18 +36,6 @@ public class LaunchApplication extends BasicExtentReport
 	public static  WebDriverWait wait = new WebDriverWait(driver, 20);
 	public static WebDriverWait shortwait=new WebDriverWait(driver,5);
 	
-	/*ChromeOptions options = new ChromeOptions();
-
-	options.addArguments("test-type");
-	options.addArguments("start-maximized");
-	options.addArguments("--js-flags=--expose-gc");
-	options.addArguments("--enable-precise-memory-info");
-	options.addArguments("--disable-popup-blocking");
-	options.addArguments("--disable-default-apps");
-	options.addArguments("test-type=browser");    
-	options.addArguments("disable-infobars");
-
-	driver = new ChromeDriver(options);*/
 	
 	/* STATIC METHOD TO LAUNCH CHROME BROWSER AND NAVIGATE TO THE FOCUS9 APPLICATION */
 	
